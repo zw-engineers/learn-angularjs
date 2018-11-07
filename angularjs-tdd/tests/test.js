5afb06029287f3d62120d6fe43e2e2e2cc89a3b5
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-11-07 08:36:18 +0000

    add beforeeach for test

--- angularjs-tdd/tests/test.js
+++ angularjs-tdd/tests/test.js
@@ -3,4 +3,12 @@ describe('Testing a Hello Pluralsight controller', () => {
     var $controller;
 
     // Set up for all tests
+    beforeEach( () => {
+        // Loads the app module
+        module('ItemsApp');
+        inject( (_$controller_) => {
+            // inject removes the underscores and finds the $controller Provider
+            $controller = _$controller_;
+        });
+    })
 });
