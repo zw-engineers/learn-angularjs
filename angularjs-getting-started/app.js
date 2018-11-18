82230855743cf51fee687e9bcf8921ca8e81c0d4
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-11-18 01:55:09 +0000

    add component

--- angularjs-getting-started/app.js
+++ angularjs-getting-started/app.js
@@ -3,3 +3,11 @@ var app = angular.module('plunker', []);
 app.controller('MainController', function($scope) { // Controller
     $scope.message = 'World'; // Model
 });
+
+app.component("output",{
+    bindings: { out: '<' },
+    template: `<div>
+    <button ng-click="$ctrl.out = $ctrl.out + 1;">buy one</button>
+    <button ng-click="$ctrl.out = $ctrl.out + 5;">buy many</button>
+  </div>`
+});
